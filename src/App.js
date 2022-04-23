14ed27c4cdccd27caf7120520ffe5e766a2d5997
Kyoo130 <kml9003@kakao.com>
2022-04-23 20:36:46 +0900

    :sparkles: Router 세팅 및 Pages Components 생성

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { Login, ProductAll, ProductDetail } from "./pages";
 
 function App() {
   return (
-    <div>
-      Clothing Store
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<ProductAll />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/product/:id" element={<ProductDetail />} />
+      </Routes>
+    </>
   );
 }
 
