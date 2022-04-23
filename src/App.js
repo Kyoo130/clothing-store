f397f1ca0f5b16045c02ee7603c1318dde11ff99
Kyoo130 <kml9003@kakao.com>
2022-04-23 21:29:56 +0900

    :sparkles: 상단 메뉴바 컴포넌트 생성

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import { Login, ProductAll, ProductDetail } from "./pages";
+import {Navbar} from "./components";
 
 function App() {
   return (
-    <>
+    <div>
+      <Navbar />
       <Routes>
         <Route path="/" element={<ProductAll />} />
         <Route path="/login" element={<Login />} />
         <Route path="/product/:id" element={<ProductDetail />} />
       </Routes>
-    </>
+    </div>
   );
 }
 
